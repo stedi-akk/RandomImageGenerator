6e6ac7696e04f2f98e8aa239e75cc68ba6fc0678
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-03-04 13:32:41 +0100

    TextOverlayGenerator and range example in sample

--- sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
+++ sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
@@ -12,6 +12,7 @@ import com.stedi.randomimagegenerator.ImageParams;
 import com.stedi.randomimagegenerator.Rig;
 import com.stedi.randomimagegenerator.callbacks.GenerateCallback;
 import com.stedi.randomimagegenerator.generators.FlatColorGenerator;
+import com.stedi.randomimagegenerator.generators.TextOverlayGenerator;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GenerateCallback {
     private ImagesAdapter adapter;
@@ -22,9 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.main_activity);
 
         findViewById(R.id.main_activity_btn_generate_1).setOnClickListener(this);
-        findViewById(R.id.main_activity_btn_generate_2).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_6).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_30).setOnClickListener(this);
+        findViewById(R.id.main_activity_btn_generate_range).setOnClickListener(this);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_activity_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -40,14 +41,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.main_activity_btn_generate_1:
                 generate(1);
                 break;
-            case R.id.main_activity_btn_generate_2:
-                generate(2);
-                break;
             case R.id.main_activity_btn_generate_6:
                 generate(6);
                 break;
             case R.id.main_activity_btn_generate_30:
                 generate(30);
+                break;
+            case R.id.main_activity_btn_generate_range:
+                generateRange();
                 break;
         }
     }
@@ -58,9 +59,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 new Rig.Builder()
                         .setCallback(MainActivity.this)
-                        .setGenerator(new FlatColorGenerator())
+                        .setGenerator(new TextOverlayGenerator(new FlatColorGenerator()))
                         .setFixedSize(600, 600)
                         .setCount(count)
+                        .build().generate();
+            }
+        }).start();
+    }
+
+    private void generateRange() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new Rig.Builder()
+                        .setCallback(MainActivity.this)
+                        .setGenerator(new TextOverlayGenerator(new FlatColorGenerator()))
+                        .setHeightRange(200, 800, 200)
+                        .setWidthRange(600, 300, 100)
                         .build().generate();
             }
         }).start();
@@ -82,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 e.printStackTrace();
-                Toast.makeText(MainActivity.this, "failed to generate", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "failed to generate", Toast.LENGTH_SHORT).show();
             }
         });
     }
