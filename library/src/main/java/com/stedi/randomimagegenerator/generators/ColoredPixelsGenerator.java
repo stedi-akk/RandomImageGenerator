4916a00d19795109749acfee205b884e2b4adc66
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-03-04 15:34:09 +0100

    added generation options in sample

---
+++ library/src/main/java/com/stedi/randomimagegenerator/generators/ColoredPixelsGenerator.java
@@ -0,0 +1,12 @@
+package com.stedi.randomimagegenerator.generators;
+
+import android.graphics.Bitmap;
+
+import com.stedi.randomimagegenerator.ImageParams;
+
+public class ColoredPixelsGenerator implements Generator {
+    @Override
+    public Bitmap generate(ImageParams imageParams) throws Exception {
+        return null;
+    }
+}
--- sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
+++ sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
@@ -1,26 +1,70 @@
 package com.stedi.randomimagegenerator.sample;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.stedi.randomimagegenerator.ImageParams;
+import com.stedi.randomimagegenerator.Quality;
 import com.stedi.randomimagegenerator.Rig;
 import com.stedi.randomimagegenerator.callbacks.GenerateCallback;
+import com.stedi.randomimagegenerator.callbacks.SaveCallback;
+import com.stedi.randomimagegenerator.generators.ColoredPixelsGenerator;
 import com.stedi.randomimagegenerator.generators.FlatColorGenerator;
+import com.stedi.randomimagegenerator.generators.Generator;
 import com.stedi.randomimagegenerator.generators.TextOverlayGenerator;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, GenerateCallback {
-    private ImagesAdapter adapter;
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener,
+        View.OnClickListener,
+        GenerateCallback,
+        SaveCallback {
+
+    private Spinner spGenerator;
+    private Spinner spQualityFormat;
+    private CheckBox cbTextOverlay;
+    private CheckBox cbSaveFile;
+    private ImagesAdapter imagesAdapter;
+
+    private Generator selectedGenerator = new FlatColorGenerator();
+    private Bitmap.CompressFormat selectedQualityFormat = Bitmap.CompressFormat.PNG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        spGenerator = (Spinner) findViewById(R.id.main_activity_sp_generator);
+        spGenerator.setOnItemSelectedListener(this);
+        spGenerator.setAdapter(createSpinnerAdapter(new String[]{
+                FlatColorGenerator.class.getSimpleName(),
+                ColoredPixelsGenerator.class.getSimpleName()
+        }));
+
+        spQualityFormat = (Spinner) findViewById(R.id.main_activity_sp_quality_format);
+        spQualityFormat.setOnItemSelectedListener(this);
+        spQualityFormat.setAdapter(createSpinnerAdapter(new String[]{
+                Bitmap.CompressFormat.PNG.name(),
+                Bitmap.CompressFormat.JPEG.name(),
+                Bitmap.CompressFormat.WEBP.name()
+        }));
+
+        cbTextOverlay = (CheckBox) findViewById(R.id.main_activity_cb_text_overlay);
+        cbSaveFile = (CheckBox) findViewById(R.id.main_activity_cb_save_file);
 
         findViewById(R.id.main_activity_btn_generate_1).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_6).setOnClickListener(this);
@@ -30,13 +74,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_activity_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        adapter = new ImagesAdapter();
-        recyclerView.setAdapter(adapter);
+        imagesAdapter = new ImagesAdapter();
+        recyclerView.setAdapter(imagesAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = (String) parent.getItemAtPosition(position);
+        if (parent == spGenerator) {
+            selectedGenerator = generatorFactory(item);
+        } else if (parent == spQualityFormat) {
+            selectedQualityFormat = qualityFormatFactory(item);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        adapter.clear();
+        imagesAdapter.clear();
         switch (v.getId()) {
             case R.id.main_activity_btn_generate_1:
                 generate(1);
@@ -48,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 generate(30);
                 break;
             case R.id.main_activity_btn_generate_range:
-                generateRange();
+                generate(-1);
                 break;
         }
     }
@@ -57,26 +111,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         new Thread(new Runnable() {
             @Override
             public void run() {
-                new Rig.Builder()
-                        .setCallback(MainActivity.this)
-                        .setGenerator(new TextOverlayGenerator(new FlatColorGenerator()))
-                        .setFixedSize(600, 600)
-                        .setCount(count)
-                        .build().generate();
-            }
-        }).start();
-    }
+                Generator generator = cbTextOverlay.isChecked() ?
+                        new TextOverlayGenerator(selectedGenerator) : selectedGenerator;
 
-    private void generateRange() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new Rig.Builder()
+                Rig.Builder builder = new Rig.Builder()
                         .setCallback(MainActivity.this)
-                        .setGenerator(new TextOverlayGenerator(new FlatColorGenerator()))
-                        .setHeightRange(200, 800, 200)
-                        .setWidthRange(600, 300, 100)
-                        .build().generate();
+                        .setGenerator(generator)
+                        .setQuality(new Quality(selectedQualityFormat, 100));
+
+                if (count > 0) {
+                    builder.setFixedSize(600, 600);
+                    builder.setCount(count);
+                } else {
+                    builder.setHeightRange(200, 800, 200);
+                    builder.setWidthRange(600, 300, 100);
+                }
+
+                if (cbSaveFile.isChecked()) {
+                    String picturesFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
+                    builder.setFileSavePath(picturesFolder + File.separator + "rig");
+                    builder.setFileSaveCallback(MainActivity.this);
+                }
+
+                builder.build().generate();
             }
         }).start();
     }
@@ -86,19 +143,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                adapter.add(bitmap);
+                imagesAdapter.add(bitmap);
             }
         });
     }
 
     @Override
-    public void onException(ImageParams imageParams, final Exception e) {
+    public void onException(final ImageParams imageParams, final Exception e) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 e.printStackTrace();
-                Toast.makeText(MainActivity.this, "failed to generate", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "failed to generate id " + imageParams.getId(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onSaved(Bitmap bitmap, File file) {
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+    }
+
+    @Override
+    public void onException(Bitmap bitmap, final File file, final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "failed to save file " + file.getPath(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private SpinnerAdapter createSpinnerAdapter(String[] items) {
+        return new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+    }
+
+    private Generator generatorFactory(String className) {
+        if (className.equals(ColoredPixelsGenerator.class.getSimpleName()))
+            return new ColoredPixelsGenerator();
+        return new FlatColorGenerator();
+    }
+
+    private Bitmap.CompressFormat qualityFormatFactory(String enumName) {
+        for (Bitmap.CompressFormat cf : Bitmap.CompressFormat.values())
+            if (cf.name().equals(enumName))
+                return cf;
+        return Bitmap.CompressFormat.PNG;
     }
 }
