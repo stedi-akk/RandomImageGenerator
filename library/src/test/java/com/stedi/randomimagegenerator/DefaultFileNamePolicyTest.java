03fc5edda72fba193791b237f5a434d75da0369e
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-03-18 12:55:27 +0100

    default file name tests

---
+++ library/src/test/java/com/stedi/randomimagegenerator/DefaultFileNamePolicyTest.java
@@ -0,0 +1,41 @@
+package com.stedi.randomimagegenerator;
+
+import android.graphics.Bitmap;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DefaultFileNamePolicyTest {
+    @Test
+    public void jpg100x100() {
+        ImageParams imageParams = new ImageParams(1, 100, 100, null, Quality.jpg(100));
+
+        DefaultFileNamePolicy namePolicy = new DefaultFileNamePolicy();
+        String name = namePolicy.getName(imageParams);
+
+        assertNotNull(name);
+        assertEquals(name, "rig_100x100_1.jpg");
+    }
+
+    @Test
+    public void png1080x1920() {
+        ImageParams imageParams = new ImageParams(10, 1080, 1920, null, Quality.png());
+
+        DefaultFileNamePolicy namePolicy = new DefaultFileNamePolicy();
+        String name = namePolicy.getName(imageParams);
+
+        assertNotNull(name);
+        assertEquals(name, "rig_1080x1920_10.png");
+    }
+
+    @Test
+    public void webp2033x10() {
+        ImageParams imageParams = new ImageParams(1337, 2033, 10, null, new Quality(Bitmap.CompressFormat.WEBP, 100));
+
+        DefaultFileNamePolicy namePolicy = new DefaultFileNamePolicy();
+        String name = namePolicy.getName(imageParams);
+
+        assertNotNull(name);
+        assertEquals(name, "rig_2033x10_1337.webp");
+    }
+}
