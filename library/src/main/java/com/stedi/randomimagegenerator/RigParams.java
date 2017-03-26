fa50bc7cdec085b4bedcfe20630b611f280a3f22
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2017-03-26 16:14:00 +0200

    javadoc for RigParams

--- library/src/main/java/com/stedi/randomimagegenerator/RigParams.java
+++ library/src/main/java/com/stedi/randomimagegenerator/RigParams.java
@@ -6,18 +6,138 @@ import com.stedi.randomimagegenerator.generators.Generator;
 
 import java.io.File;
 
+/**
+ * Generation parameters created by {@link Rig.Builder} and used by {@link Rig}
+ */
 class RigParams {
+    /**
+     * Used for bitmap generation.
+     * <p>
+     * Must not be null.
+     * </p>
+     */
     Generator generator;
+
+    /**
+     * Callbacks while generation.
+     * <p>
+     * Can be null.
+     * </p>
+     */
     GenerateCallback generateCallback;
+
+    /**
+     * Used by {@link Generator} and for image saving quality
+     * <p>
+     * Must not be null.
+     * </p>
+     */
     Quality quality;
-    int width, height;
-    int widthFrom, widthTo, widthStep;
-    int heightFrom, heightTo, heightStep;
+
+    /**
+     * Width of generated image.
+     * <p>
+     * Must be > 0, if width range is not specified.
+     * </p>
+     */
+    int width;
+
+    /**
+     * Height of generated image.
+     * <p>
+     * Must be > 0, if height range is not specified.
+     * </p>
+     */
+    int height;
+
+    /**
+     * Width range start. Used along with widthTo and widthStep.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int widthFrom;
+
+    /**
+     * Width range end. Used along with widthFrom and widthStep.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int widthTo;
+
+    /**
+     * Width range step. Used along with widthFrom and widthTo.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int widthStep;
+
+    /**
+     * Height range start. Used along with heightTo and heightStep.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int heightFrom;
+
+    /**
+     * Height range end. Used along with heightFrom and heightStep.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int heightTo;
+
+    /**
+     * Height range step. Used along with heightFrom and heightTo.
+     * <p>
+     * Must be > 0, if range is specified.
+     * </p>
+     */
+    int heightStep;
+
+    /**
+     * true if width range is specified.
+     */
     boolean useWidthRange;
+
+    /**
+     * true if height range is specified.
+     */
     boolean useHeightRange;
+
+    /**
+     * The number of generated images.
+     * <p>
+     * Must be > 0 for fixed size.
+     * </p>
+     */
     int count;
+
+    /**
+     * The path where images will be saved.
+     * <p>
+     * Can be null.
+     * </p>
+     */
     File path;
+
+    /**
+     * File name used for image save.
+     * <p>
+     * Must not be null if path is specified.
+     * </p>
+     */
     FileNamePolicy fileNamePolicy;
+
+    /**
+     * Callbacks while image save.
+     * <p>
+     * Can be null.
+     * </p>
+     */
     SaveCallback saveCallback;
 
     void apply(RigParams from) {
