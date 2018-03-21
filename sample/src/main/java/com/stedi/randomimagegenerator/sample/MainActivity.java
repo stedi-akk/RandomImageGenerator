b1a7ab9d8d0ff248fb9302f86b3af895afafb419
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-03-21 14:22:15 +0100

    sample update

--- sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
+++ sample/src/main/java/com/stedi/randomimagegenerator/sample/MainActivity.java
@@ -3,8 +3,10 @@ package com.stedi.randomimagegenerator.sample;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,7 +15,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.stedi.randomimagegenerator.ImageParams;
@@ -22,15 +23,20 @@ import com.stedi.randomimagegenerator.Rig;
 import com.stedi.randomimagegenerator.callbacks.GenerateCallback;
 import com.stedi.randomimagegenerator.callbacks.SaveCallback;
 import com.stedi.randomimagegenerator.generators.ColoredCirclesGenerator;
+import com.stedi.randomimagegenerator.generators.ColoredLinesGenerator;
 import com.stedi.randomimagegenerator.generators.ColoredNoiseGenerator;
 import com.stedi.randomimagegenerator.generators.ColoredPixelsGenerator;
 import com.stedi.randomimagegenerator.generators.ColoredRectangleGenerator;
 import com.stedi.randomimagegenerator.generators.FlatColorGenerator;
 import com.stedi.randomimagegenerator.generators.Generator;
-import com.stedi.randomimagegenerator.generators.MirroredGenerator;
-import com.stedi.randomimagegenerator.generators.TextOverlayGenerator;
+import com.stedi.randomimagegenerator.generators.GeneratorPack;
+import com.stedi.randomimagegenerator.generators.effects.MirroredEffect;
+import com.stedi.randomimagegenerator.generators.effects.TextOverlayEffect;
+import com.stedi.randomimagegenerator.generators.effects.ThresholdEffect;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener,
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements
     private Spinner spQualityFormat;
     private CheckBox cbTextOverlay;
     private CheckBox cbMirrored;
+    private CheckBox cbThreshold;
     private CheckBox cbSaveFile;
     private RecyclerView recyclerView;
     private ImagesAdapter imagesAdapter;
@@ -55,34 +62,37 @@ public class MainActivity extends AppCompatActivity implements
         Rig.enableDebugLogging(true);
         setContentView(R.layout.main_activity);
 
-        spGenerator = (Spinner) findViewById(R.id.main_activity_sp_generator);
+        spGenerator = findViewById(R.id.main_activity_sp_generator);
         spGenerator.setOnItemSelectedListener(this);
-        spGenerator.setAdapter(createSpinnerAdapter(new String[]{
-                FlatColorGenerator.class.getSimpleName(),
-                ColoredPixelsGenerator.class.getSimpleName(),
-                ColoredNoiseGenerator.class.getSimpleName(),
-                ColoredCirclesGenerator.class.getSimpleName(),
-                ColoredRectangleGenerator.class.getSimpleName()
+        spGenerator.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{
+                "GeneratorPack",
+                "FlatColorGenerator",
+                "ColoredPixelsGenerator",
+                "ColoredNoiseGenerator",
+                "ColoredCirclesGenerator",
+                "ColoredRectangleGenerator",
+                "ColoredLinesGenerator"
         }));
 
-        spQualityFormat = (Spinner) findViewById(R.id.main_activity_sp_quality_format);
+        spQualityFormat = findViewById(R.id.main_activity_sp_quality_format);
         spQualityFormat.setOnItemSelectedListener(this);
-        spQualityFormat.setAdapter(createSpinnerAdapter(new String[]{
+        spQualityFormat.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{
                 Bitmap.CompressFormat.PNG.name(),
                 Bitmap.CompressFormat.JPEG.name(),
                 Bitmap.CompressFormat.WEBP.name()
         }));
 
-        cbTextOverlay = (CheckBox) findViewById(R.id.main_activity_cb_text_overlay);
-        cbMirrored = (CheckBox) findViewById(R.id.main_activity_cb_mirrored);
-        cbSaveFile = (CheckBox) findViewById(R.id.main_activity_cb_save_file);
+        cbTextOverlay = findViewById(R.id.main_activity_cb_text_overlay);
+        cbMirrored = findViewById(R.id.main_activity_cb_mirrored);
+        cbThreshold = findViewById(R.id.main_activity_cb_threshold);
+        cbSaveFile = findViewById(R.id.main_activity_cb_save_file);
 
         findViewById(R.id.main_activity_btn_generate_1).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_6).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_30).setOnClickListener(this);
         findViewById(R.id.main_activity_btn_generate_range).setOnClickListener(this);
 
-        recyclerView = (RecyclerView) findViewById(R.id.main_activity_rv);
+        recyclerView = findViewById(R.id.main_activity_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         imagesAdapter = new ImagesAdapter();
@@ -119,15 +129,23 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void generate(final int count) {
-        new Thread(new Runnable() {
+        AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
                 Generator generator = selectedGenerator;
-                if (cbMirrored.isChecked())
-                    generator = new MirroredGenerator(generator);
-                if (cbTextOverlay.isChecked())
-                    generator = new TextOverlayGenerator.Builder()
+
+                if (cbThreshold.isChecked()) {
+                    generator = new ThresholdEffect(generator);
+                }
+
+                if (cbMirrored.isChecked()) {
+                    generator = new MirroredEffect(generator);
+                }
+
+                if (cbTextOverlay.isChecked()) {
+                    generator = new TextOverlayEffect.Builder()
                             .setGenerator(generator).build();
+                }
 
                 Rig.Builder builder = new Rig.Builder()
                         .setCallback(MainActivity.this)
@@ -150,11 +168,11 @@ public class MainActivity extends AppCompatActivity implements
 
                 builder.build().generate();
             }
-        }).start();
+        });
     }
 
     @Override
-    public void onGenerated(ImageParams imageParams, final Bitmap bitmap) {
+    public void onGenerated(@NonNull ImageParams imageParams, @NonNull final Bitmap bitmap) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -165,7 +183,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onFailedToGenerate(final ImageParams imageParams, final Exception e) {
+    public void onFailedToGenerate(@NonNull final ImageParams imageParams, @NonNull final Exception e) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -176,12 +194,12 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onSaved(Bitmap bitmap, File file) {
+    public void onSaved(@NonNull Bitmap bitmap, @NonNull File file) {
         sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
     }
 
     @Override
-    public void onFailedToSave(Bitmap bitmap, final Exception e) {
+    public void onFailedToSave(@NonNull Bitmap bitmap, @NonNull final Exception e) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -191,31 +209,44 @@ public class MainActivity extends AppCompatActivity implements
         });
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    private SpinnerAdapter createSpinnerAdapter(String[] items) {
-        return new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-    }
-
     private Generator generatorFactory(String className) {
-        if (className.equals(ColoredPixelsGenerator.class.getSimpleName()))
-            return new ColoredPixelsGenerator(20);
-        if (className.equals(ColoredNoiseGenerator.class.getSimpleName()))
-            return new ColoredNoiseGenerator();
-        if (className.equals(ColoredCirclesGenerator.class.getSimpleName()))
-            return new ColoredCirclesGenerator();
-        if (className.equals(ColoredRectangleGenerator.class.getSimpleName()))
-            return new ColoredRectangleGenerator();
-        return new FlatColorGenerator();
+        switch (className) {
+            case "GeneratorPack":
+                return new GeneratorPack(new ArrayList<>(Arrays.asList(new Generator[]{
+                        generatorFactory("ColoredPixelsGenerator"),
+                        generatorFactory("ColoredNoiseGenerator"),
+                        generatorFactory("ColoredCirclesGenerator"),
+                        generatorFactory("ColoredRectangleGenerator"),
+                        generatorFactory("ColoredLinesGenerator"),
+                })));
+            case "FlatColorGenerator":
+                return new FlatColorGenerator();
+            case "ColoredPixelsGenerator":
+                return new ColoredPixelsGenerator(20);
+            case "ColoredNoiseGenerator":
+                return new ColoredNoiseGenerator();
+            case "ColoredCirclesGenerator":
+                return new ColoredCirclesGenerator();
+            case "ColoredRectangleGenerator":
+                return new ColoredRectangleGenerator();
+            case "ColoredLinesGenerator":
+                return new ColoredLinesGenerator();
+            default:
+                throw new IllegalStateException();
+        }
     }
 
     private Bitmap.CompressFormat qualityFormatFactory(String enumName) {
-        for (Bitmap.CompressFormat cf : Bitmap.CompressFormat.values())
-            if (cf.name().equals(enumName))
+        for (Bitmap.CompressFormat cf : Bitmap.CompressFormat.values()) {
+            if (cf.name().equals(enumName)) {
                 return cf;
-        return Bitmap.CompressFormat.PNG;
+            }
+        }
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
